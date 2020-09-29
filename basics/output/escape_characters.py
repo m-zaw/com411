c170826b2f23991feafb5e6816dce8f4c44cdd66
Milan <64641136+m-zaw@users.noreply.github.com>
2020-09-29 12:12:16 +0100

    escape_characters.py task update

--- basics/output/escape_characters.py
+++ basics/output/escape_characters.py
@@ -4,3 +4,6 @@ print("\t Displays a tab space")
 print("\\ Displays a back slash")
 print("\" Displays a double quote")
 print("\' Displays a single quote")
+# Task
+print("\n")
+print("\t I am Beep!")
