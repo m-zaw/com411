d79c3e6a19cbcdfd98d111f912052d21dfe295a2
Milan <64641136+m-zaw@users.noreply.github.com>
2020-09-29 12:51:43 +0100

    string_operators.py added with task

---
+++ basics/input/string_operators.py
@@ -0,0 +1,14 @@
+print("Please enter the number of lives.")
+lives = int(input())
+
+print("Please enter the energy level.")
+energy = int(input())
+
+print("Please enter the shield level.")
+shield = int(input())
+
+print("Health has been set.")
+
+print("Lives:\t", "♥" * lives)
+print("Energy:\t", "♦" * energy)
+print("Shield:\t", "♦" * shield)
