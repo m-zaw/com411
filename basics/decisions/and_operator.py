5f1087b5c9d9a1ca0a62fc2c25de67e6c44a9024
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-06 12:15:04 +0100

    added and_operator.py

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,10 @@
+print("What did I hear?")
+hear = input() 
+
+print("What did I see?")
+see = input()
+
+if ( (hear == "grr") and (see == "two red eyes") ):
+    print("\nThere is a scary creature. I should get out of here!")
+else:
+    print("\nI am a little scared but I will continue.")
