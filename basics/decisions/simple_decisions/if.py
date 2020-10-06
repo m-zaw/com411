3fc2ef223f1421b98531b5e21398749bb5e16558
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-06 10:56:31 +0100

    added if.py and if_else.py

---
+++ basics/decisions/simple_decisions/if.py
@@ -0,0 +1,7 @@
+print("What type of book is this?")
+book_genre = input() 
+
+if (book_genre == "adventure"):
+    print("\nI like adventure books!")
+
+print("\nFinished reading book.")
---
+++ basics/decisions/simple_decisions/if_else.py
@@ -0,0 +1,9 @@
+print("Please enter the activity to be performed.")
+activity = input() 
+
+if (activity == "calculate"):
+    print("\nPerforming calculations...")
+else:
+    print("\nPerforming activity...")
+
+print("\nActivity completed.")
