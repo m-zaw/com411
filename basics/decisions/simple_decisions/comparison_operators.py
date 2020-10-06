3d7721fc2431a6cd5b20f3352d10dd008ed835a0
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-06 11:08:59 +0100

    added comparison_operators.py

---
+++ basics/decisions/simple_decisions/comparison_operators.py
@@ -0,0 +1,12 @@
+print("Please enter the first number.")
+a = int(input()) 
+
+print("Please enter the second number.")
+b = int(input())
+
+if (a < b):
+    print("\nThe first number is the smallest.")
+elif (a > b):
+    print("\nThe second number is the smallest.")
+else:
+    print("\nThe two numbers are equal.")
