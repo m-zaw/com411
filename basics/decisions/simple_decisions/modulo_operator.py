1d781c73134d3f4ac1045de33aaa127610846a56
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-06 11:06:34 +0100

    added modulo_operator.py

---
+++ basics/decisions/simple_decisions/modulo_operator.py
@@ -0,0 +1,7 @@
+print("Please enter a whole number.")
+number = int(input()) 
+
+if (number % 2 == 0):
+    print("\nThe number {} is an even number.".format(number))
+else:
+    print("\nThe number {} is an odd number.".format(number))
