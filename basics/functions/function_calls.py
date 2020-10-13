830cf2a9665ea34f4c063bb8d52884e63f293930
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-13 13:00:22 +0100

    added function_calls.py

--- basics/functions/function_calls.py
+++ basics/functions/function_calls.py
@@ -1,2 +1,22 @@
-def display_box():
-    
+def box(word):
+    num_dashes = 6 + len(word)
+    print("-" * num_dashes)
+    print("|  {}  |".format(word))
+    print("-" * num_dashes)
+
+def uppercase(word):
+    print(word.upper())
+
+def lowercase(word):
+    print(word.lower())
+
+def mirror(word):
+    reverse_word = ""
+    for letter in reversed(word):
+        reverse_word += letter
+    print("{} | {}".format(word, reverse_word))
+
+box("hello world")
+uppercase("hello world")
+lowercase("hello world")
+mirror("hello world")
