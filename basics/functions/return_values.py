48c9c2f24875eea7e7f5a6cbedef67fb5a79b648
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-13 12:49:35 +0100

    return_values.py added

---
+++ basics/functions/return_values.py
@@ -0,0 +1,28 @@
+def sum_weight(beep_weight, bop_weight):
+    total_weight = beep_weight + bop_weight
+    return total_weight
+
+def avg_weight(beep_weight, bop_weight):
+    average = (beep_weight + bop_weight) / 2
+    return average
+
+def run():
+    print("What is the weight of Beep?")
+    beep_weight = float(input())
+
+    print("What is the weight of Bop?")
+    bop_weight = float(input())
+
+    print("What would you like to calculate (sum or average)?")
+    action = input()
+
+    if (action == "sum"):
+        answer = sum_weight(beep_weight, bop_weight)
+        print("The sum of Beep and Bop's weight is {}.".format(answer))
+    elif(action == "average"):
+        answer = avg_weight(beep_weight, bop_weight)
+        print("The avg of Beep and Bop's weight is {}.".format(answer))
+    else:
+        print("Ation not recognised")
+
+run()
