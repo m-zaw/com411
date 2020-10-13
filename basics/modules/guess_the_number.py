85115415d32351e4c588bb9d2cd5ca03beeb1b0c
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-13 14:02:20 +0100

    added code to import and use of a module

---
+++ basics/modules/guess_the_number.py
@@ -0,0 +1,26 @@
+import random as rnd
+
+print("Please enter the minimum value:")
+min_num = int(input())
+
+print("Please enter the maximum value:")
+max_num = int(input())
+r_num = rnd.randrange(min_num, max_num, 1)
+
+print("I am thinking of a number between {} and {}. Can you guess what it is?".format(min_num,max_num))
+
+def play_guess_the_number():
+    guess_num = int(input())
+
+    if guess_num == r_num:
+        print("Congratulations! You guessed my number!")
+    elif guess_num > r_num:
+        print("Your guess is too high.")
+        print("Try again:")
+        play_guess_the_number()
+    elif guess_num < r_num:
+        print("Your guess is too low.")
+        print("Try again:")
+        play_guess_the_number()
+
+play_guess_the_number()
---
+++ basics/test.py
@@ -0,0 +1,3 @@
+import random as rnd
+
+print(rnd.randrange(1, 10, 1))
