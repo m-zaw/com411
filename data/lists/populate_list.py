11a3b4f9a578a2c1e85327b26979eeb3b0c3c1c2
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-20 12:22:21 +0100

    update

--- data/lists/populate_list.py
+++ data/lists/populate_list.py
@@ -5,15 +5,15 @@ def directions():
 def menu():
     print("Please select a direction:")
     direction = directions()
-    for x in range(len(direction)):
-        print("{0}:{1}".format(x,direction[x]))
+    for a in range(len(direction)):
+        print("{0}:{1}".format(a,direction[a]))
     user_choice = int(input())
     return direction[user_choice]
 
 def run():
     route = []
     print("Working out escape route...")
-    for x in range(5):
+    for a in range(5):
         user_choice = menu()
         route.append(user_choice)
     print("Escape Route: {}".format(route))
