4a901d817652eac29b9ce26da71350129321c57b
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-20 12:38:53 +0100

    added nested_tuple.py

---
+++ data/tuples/nested_tuple.py
@@ -0,0 +1,20 @@
+def steps():
+    percentages = [50, 38, 27, 99, 4]
+    likelihoods = []
+    for a in range (len(percentages)):
+        step = "step " + str(a+1)
+        step_likelihood = (step, percentages[a])
+        likelihoods.append(step_likelihood)
+    return likelihoods
+
+def run():
+    good_steps = []
+    bad_steps = []
+    for a in range (len(steps())):
+        if steps()[a][-1] >= 50:
+            bad_steps.append(steps()[a])
+    else:
+        good_steps.append(steps()[a])
+    print("Good steps: {}, Bad steps: {}".format(len(good_steps), len(bad_steps)))
+
+run()
