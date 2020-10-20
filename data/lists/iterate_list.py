d7223eddad1769fc592d0e29b15d09a89ed17cde
Milan <64641136+m-zaw@users.noreply.github.com>
2020-10-20 11:20:57 +0100

    /data/lists folder update with its files

---
+++ data/lists/iterate_list.py
@@ -0,0 +1,14 @@
+def directions():
+    directions = ["Move Forward", "Move Backwards", "Turn Left", "Turn Right"]
+    return directions 
+
+def menu():
+    print("please select a direction:")
+    direction = directions()
+    for index in range(len(direction)):
+        print("{}: {}".format(index, direction[index]))
+
+def run():
+    menu()
+
+run()
