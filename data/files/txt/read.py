1c7b002e905745fbf42fce69aa6cd072b55f6105
Milan <64641136+m-zaw@users.noreply.github.com>
2020-11-03 13:18:58 +0000

    Added code to demonstrate how to write to a text file.

--- data/files/txt/read.py
+++ data/files/txt/read.py
@@ -1,16 +1,13 @@
-filepath = "C:/Users/Milan/OneDrive - Solent University/SOLENT_L6/Y1/com411/data/files/txt/locations.txt"
-
-
 def search(filename):
     print("Searching...")
     with open(filename) as file:
         for line in file:
             print(f"Looked in {line}.", end="")
-    print('\n'"...Done!")
+    print("\n...Done!")
 
 
 def run():
-    search(filepath)
+    search("data/files/txt/locations.txt")
 
 
 run()
---
+++ data/files/txt/write.py
@@ -0,0 +1,39 @@
+def search(filename):
+    print("Searching... ", end="")
+
+    sections = []
+    books = []
+
+    with open(filename) as file:
+        for line in file:
+            if line.startswith("Section"):
+                parts = line.split(":")
+                sections.append(parts[1].replace("\n", ""))
+            else:
+                books.append(line.replace("\n", ""))
+
+    print("Done!")
+    return (sections, books)
+
+
+def save(filename, data):
+    print("Saving... ", end="")
+
+    with open(filename, "w") as file:
+        file.write("Sections: ")
+        for section in data[0]:
+            file.write(f"{section},")
+
+        file.write("\nBooks: ")
+        for books in data[1]:
+            file.write(f"{books},")
+    print("Done!")
+
+
+def run():
+
+    data = search("data/files/txt/books.txt")
+    save("data/files/txt/section-books.txt", data)
+
+
+run()
