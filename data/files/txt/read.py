0b3d46802a317f0dbd9efa582f88d78a6f3cabc3
Milan <64641136+m-zaw@users.noreply.github.com>
2020-11-03 10:53:20 +0000

    Added code to demonstrate how to read a text file.

---
+++ data/files/txt/read.py
@@ -0,0 +1,12 @@
+filepath = "C:/Users/Milan/OneDrive - Solent University/SOLENT_L6/Y1/com411/data/files/txt/locations.txt"
+def search(filename):
+  print("Searching...")
+  with open(filename) as file:
+    for line in file:
+      print(f"Looked in {line}.", end="")
+  print('\n'"...Done!")
+
+def run():
+  search(filepath)
+
+run()
