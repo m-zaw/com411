c372ed50fdceeec9f1979b0a5cf4bd7b047adbf3
Milan <64641136+m-zaw@users.noreply.github.com>
2020-11-10 10:15:05 +0000

    Added code to display a simple line plot.

---
+++ visual/plots/simple.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def display(x_list, y_list):
+    plt.xlabel("x values")
+    plt.ylabel("y values")
+    plt.plot(x_list, y_list)
+    plt.show()
+
+
+def run():
+    x_values = [1, 2, 3, 4, 5]
+    y_values = [1, 4, 9, 16, 25]
+    display(x_values, y_values)
+
+
+run()
