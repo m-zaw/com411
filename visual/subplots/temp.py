f6b6178f7a13bae7ff49710bf57e74d5c676af03
Milan <64641136+m-zaw@users.noreply.github.com>
2020-11-17 10:38:12 +0000

    Added code to display simple subplots.

---
+++ visual/subplots/temp.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+
+def read_data(file_name):
+    temps = []
+    with open(file_name) as file:
+        for line in file:
+            temps.append(float(line.replace("\n", "")))
+    return temps
+
+
+def run():
+    data = read_data("visual/subplots/temps.txt")
+    num = list(range(1, len(data)+1))
+    fig, axs = plt.subplots(1, 2)
+    axs[0].set_xlabel("Day")
+    axs[0].set_ylabel("Temperature")
+    axs[0].plot(num, data)
+
+    axs[1].set_xlabel("Day")
+    axs[1].set_ylabel("Temperature")
+    axs[1].bar(num, data)
+    plt.show()
+
+
+run()
