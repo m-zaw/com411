a1299555d6b0c9f1320069ebab831d81aa81418f
Milan <64641136+m-zaw@users.noreply.github.com>
2020-12-05 01:29:20 +0000

    Refreshed the formatting

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ import basics.output.simple_message as simple_message
 def run_block_a():
     print("Which program in 'Block A: Basics' do you wish to run?")
     response = input()
-    if (response == "simple_message"):
+    if response == "simple_message":
         simple_message.run()
-    elif (response == "multiline_message"):
+    elif response == "multiline_message":
         multiline_message.run()
 
 
 def run():
     is_running = True
 
-    while (is_running):
+    while is_running:
         print("What would you like to do?")
         print("[a] Run 'Block A: Basics' programs")
         print("[q] Quit")
         response = input()
 
-        if (response == "a"):
+        if response == "a":
             run_block_a()
-        elif (response == "q"):
+        elif response == "q":
             break
         else:
             print("Invalid option! Please try again.")
