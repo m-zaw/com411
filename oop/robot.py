46ab889d354066cbd0489045547b127383d190f7
Milan <64641136+m-zaw@users.noreply.github.com>
2020-12-08 09:55:39 +0000

    Update human, robot, planet, universe in sync with Prins's solutions.

--- oop/robot.py
+++ oop/robot.py
@@ -1,36 +1,63 @@
 class Robot:
-    LAWS = "Protect, Obey and Survive"
 
+    # class attribute
+    laws = "Protect, Obey and Survive"
+
+    # class (constant) attribute
+    MAX_ENERGY = 100
+
+    # A static method
+    @staticmethod
     def the_laws():
-        print(Robot.LAWS)
+        print(Robot.laws)
 
+    # An initialiser (special instance method)
     def __init__(self, name="Robot", age=0):
+
+        # An instance attribute
         self.name = name
         self.age = age
+        self.energy = Robot.MAX_ENERGY
 
+    def __repr__(self):
+        return f'robot(name={self.name}, age={self.age}, energy={self.energy})'
+
+    def __str__(self):
+        return f'My name is {self.name} and I am {self.age} years old and my energy is {self.energy}.'
+
+    # An instance method
     def display(self):
-        print("I am {}!".format(self.name))
+        print(f"I am {self.name}")
+
+    def eat(self, amount):
+        potential_energy = self.energy + amount
+        if (potential_energy > Robot.MAX_ENERGY):
+            self.energy = Robot.MAX_ENERGY
+            return potential_energy - self.energy
+        else:
+            self.energy = potential_energy
+            return 0
 
     def grow(self):
         self.age += 1
 
-    def eat(self, amount):
-        print("I'm a robot, I cannot eat {} food.".format(amount))
-
     def move(self, distance):
-        print("Moved {}, I'm a robot with infinite energy.".format(distance))
-
-    def __repr__(self):
-        return "robot(name={}, age={})".format(self.name, self.age)
-
-    def __str__(self):
-        return self.name
+        potential_energy = self.energy - distance
+        if (potential_energy < 0):
+            self.energy = 0
+            return self.energy - abs(potential_energy)
+        else:
+            self.energy = potential_energy
+            return 0
 
 
 if (__name__ == "__main__"):
-    beep = Robot("Beep")
-    beep.display()
-
+    robot = Robot()
     Robot.the_laws()
-
-    print(beep)
+    print(repr(robot))
+    robot.move(10)
+    print(repr(robot))
+    robot.eat(5)
+    print(repr(robot))
+    robot.eat(20)
+    print(repr(robot))
