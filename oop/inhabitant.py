8034880b94583c909e331f0c6be98be0e1604912
Milan <64641136+m-zaw@users.noreply.github.com>
2020-12-08 10:52:15 +0000

    Updated the classes Human and Robot to inherit from the class Inhabitant.

---
+++ oop/inhabitant.py
@@ -0,0 +1,22 @@
+class Inhabitant:
+
+    MAX_ENERGY = 100
+
+    def __init__(self, name="Inhabitant", age=0, energy=MAX_ENERGY):
+        pass
+
+    def grow(self):
+        self.age = self.age + 1
+
+    def display(self):
+        print(f"I am {self.name}")
+
+    def eat(self, amount):
+        if(self.energy + amount > Inhabitant.MAX_ENERGY):
+            self.energy = Inhabitant.MAX_ENERGY
+        else:
+            self.energy += amount
+
+    def move(self, distance):
+        potential_energy = self.energy - distance
+        self.energy = max(potential_energy, 0)
--- oop/robot.py
+++ oop/robot.py
@@ -1,4 +1,7 @@
-class Robot:
+from inhabitant import Inhabitant
+
+
+class Robot(Inhabitant):
 
     # class attribute
     laws = "Protect, Obey and Survive"
@@ -28,27 +31,6 @@ class Robot:
     # An instance method
     def display(self):
         print(f"I am {self.name}")
-
-    def eat(self, amount):
-        potential_energy = self.energy + amount
-        if (potential_energy > Robot.MAX_ENERGY):
-            self.energy = Robot.MAX_ENERGY
-            return potential_energy - self.energy
-        else:
-            self.energy = potential_energy
-            return 0
-
-    def grow(self):
-        self.age += 1
-
-    def move(self, distance):
-        potential_energy = self.energy - distance
-        if (potential_energy < 0):
-            self.energy = 0
-            return self.energy - abs(potential_energy)
-        else:
-            self.energy = potential_energy
-            return 0
 
 
 if (__name__ == "__main__"):
