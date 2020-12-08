ca76314dd1b4567bfe2cb9ce9275417dbc00153e
Milan <64641136+m-zaw@users.noreply.github.com>
2020-12-08 11:24:03 +0000

    Updated classes human and robot with instance methods.

--- oop/inheritance/inhabitant.py
+++ oop/inheritance/inhabitant.py
@@ -1,19 +1,46 @@
+# The Inhabitant class will be the super class for Human and Robot, it will share common
+# methods between them which are similar to the ones from last week.
 class Inhabitant:
-
     MAX_ENERGY = 100
 
-    def __init__(self, name="Inhabitant", age=0, energy=MAX_ENERGY):
-        pass
-
-    def grow(self):
-        self.age = self.age + 1
+    # When specifying a global variable inside __init__, we'd do something like MAX_ENERGY=100
+    def __init__(self, name="Inhabitant", age=0):
+        self.name = name
+        self.age = age
+        self.energy = Inhabitant.MAX_ENERGY
 
     def eat(self, amount):
-        if(self.energy + amount > Inhabitant.MAX_ENERGY):
-            self.energy = Inhabitant.MAX_ENERGY
+        temp = self.energy + amount
+
+        if (temp > 100):
+            self.energy = 100
+            print("I can't eat more, I'm full!".format)
         else:
-            self.energy += amount
+            self.energy = temp
+            print("That was yummy! My energy is now at {}!".format(self.energy))
+
+    def grow(self):
+        self.age += 1
+        print("It's my birthday! I'm now {} years old.".format(self.age))
 
     def move(self, distance):
-        potential_energy = self.energy - distance
-        self.energy = max(potential_energy, 0)
+        temp = self.energy - distance
+
+        if (temp < 0):
+            self.energy = 0
+            print("I couldn't make it!")
+        else:
+            self.energy = temp
+            print("I made it to the destination! My energy is now {}".format(self.energy))
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "inhabitant=(name={}, age={}, energy={})".format(self.name, self.age, self.energy)
+
+
+if (__name__ == "__main__"):
+    test = Inhabitant("test", 10)
+    print(test)
+    print(repr(test))
--- oop/inheritance/planet.py
+++ oop/inheritance/planet.py
@@ -1,38 +1,65 @@
+# Import the human and robot classes
 from human import Human
 from robot import Robot
 
+# Planet class which will hold the human and robot data
+
 
 class Planet:
+    def __init__(self, planet_name=None):
+        self.planet = planet_name
+        # Empty humans and robots list which will hold the human AND robot objects
+        self.inhabitants = []
 
-    def __init__(self):
-        self.inhabitants = {
-            'humans': [],
-            'robots': []
-        }
+    # Add inhabitant method which appends the inhabitant to the inhabitants list
+    def add_inhabitant(self, inhabitant_to_add):
+        self.inhabitants.append(inhabitant_to_add)
+
+    # Remove inhabitant method which checks if the inhabitant exists, removes if true
+    def remove_inhabitant(self, inhabitant_to_remove):
+        if (inhabitant_to_remove in self.inhabitants):
+            self.inhabitants.remove(inhabitant_to_remove)
+            print("Removed inhabitant {}".format(inhabitant_to_remove))
+        else:
+            print("The inhabitant doesn't exist")
+
+    def count_inhabitants(self):
+        total_humans, total_robots = 0, 0
+
+        for inhabitant in self.inhabitants:
+            if isinstance(inhabitant, Human):
+                total_humans += 1
+            else:
+                total_robots += 1
+
+        print("There are {} humans and {} robots.".format(
+            total_humans, total_robots))
 
     def __repr__(self):
-        return f"planet(humans={self.inhabitants['humans']}, robots={self.inhabitants['robots']})"
+        return "{}=(inhabitants={})".format(self.planet, self.inhabitants)
 
     def __str__(self):
-        return f"This planet has {len(self.inhabitants['humans'])} humans and {len(self.inhabitants['robots'])} robots."
-
-    def add_human(self, human):
-        self.inhabitants['humans'].append(human)
-
-    def add_robot(self, robot):
-        self.inhabitants['robots'].append(robot)
-
-    def remove_human(self, human):
-        self.inhabitants['humans'].remove(human)
-
-    def remove_robot(self, robot):
-        self.inhabitants['robots'].remove(robot)
+        return "### {} ### \nInhabitants: {}".format(self.planet, self.inhabitants)
 
 
 if (__name__ == "__main__"):
-    planet = Planet()
-    print(repr(planet))
-    prins = Human("Prins")
-    planet.add_human(prins)
-    print(repr(planet))
-    print(planet)
+    homeworld = Planet("Earth")
+
+    human1 = Human("Harry")
+    human2 = Human("Ron")
+
+    homeworld.add_inhabitant(human1)
+    homeworld.add_inhabitant(human2)
+    homeworld.remove_inhabitant(human1)
+
+    print(homeworld)
+
+    robot1 = Robot("C3PO")
+    robot2 = Robot("R2D2")
+
+    homeworld.add_inhabitant(robot1)
+    homeworld.add_inhabitant(robot2)
+
+    print(homeworld)
+
+    homeworld.count_inhabitants()
